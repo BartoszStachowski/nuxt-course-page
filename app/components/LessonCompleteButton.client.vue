b852be8602439d0b034a38592482dfab56d8ee93
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-08-04 08:50:47 +0200

    Initial commit

---
+++ app/components/LessonCompleteButton.client.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const modelValue = defineModel<boolean>();
+</script>
+
+<template>
+  <label
+    class="cursor-pointer rounded px-4 py-2 font-bold text-white"
+    :class="{
+      'bg-green-500': modelValue,
+      'bg-gray-500': !modelValue,
+    }"
+  >
+    <input type="checkbox" v-model="modelValue" class="hidden" />
+    {{ modelValue ? "Completed!" : "Mark as complete" }}
+  </label>
+</template>
+
+<style scoped>
+::selection {
+  display: none;
+}
+</style>
---
+++ app/components/VideoPlayer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  videoId: number;
+}>();
+</script>
+
+<template>
+  <iframe
+    class="aspect-video w-full rounded"
+    :src="`https://player.vimeo.com/video/${videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
